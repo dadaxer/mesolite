50315523cdd3e4276ef48f72f7e93fea961aa7c1
dadaxer <dadaxer@mailbox.org>
2025-07-23 15:44:44 +0200

    Update bling.sh

--- files/scripts/bling.sh
+++ files/scripts/bling.sh
@@ -7,9 +7,13 @@ set -oue pipefail
 
 # Your code goes here.
 echo 'Removing Karl...'
-
 rm /usr/share/ublue-os/bluefin-logos/karl.png
 rm /usr/share/ublue-os/bluefin-logos/symbols/karl
 rm /usr/share/ublue-os/bluefin-logos/sixels/karl
-
 echo 'Karl removed'
+
+echo 'Removing vscode files...'
+rm -rf /etc/skel/.config/Code
+rm /etc/yum.repos.d/vscode.repo
+rm /usr/share/ublue-os/user-setup.hooks.d/10-vscode.sh
+echo 'files removed'
