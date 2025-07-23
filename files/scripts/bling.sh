facc824eeb885a9d8338c0f004979bdeeb342230
dadaxer <dadaxer@mailbox.org>
2025-07-23 11:29:18 +0200

    First commit

---
+++ files/scripts/bling.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Removing karl...'
+
+rm /usr/share/ublue-os/bluefin-logos/karl.png
+rm /usr/share/ublue-os/bluefin-logos/symbols/karl
+rm /usr/share/ublue-os/bluefin-logos/sixels/karl
---
+++ files/scripts/netbird.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'This is an example shell script'
+echo 'Scripts here will run during build if specified in recipe.yml'
+
+tee /etc/yum.repos.d/netbird.repo <<EOF
+[netbird]
+name=netbird
+baseurl=https://pkgs.netbird.io/yum/
+enabled=1
+gpgcheck=0
+gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
+repo_gpgcheck=1
+EOF
+
+dnf config-manager addrepo -y --overwrite --from-repofile=/etc/yum.repos.d/netbird.repo
+dnf install netbird -y
