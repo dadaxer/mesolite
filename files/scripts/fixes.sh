c5566d7fb542ee5d752811319d3d2da5b3f89e84
dadaxer <dadaxer@mailbox.org>
2025-07-25 22:56:20 +0200

    Update fixes.sh

--- files/scripts/fixes.sh
+++ files/scripts/fixes.sh
@@ -2,4 +2,5 @@
 
 set -oue pipefail
 
+# gnome-extensions module can't remove "Window List", so manual deletion it is.
 rm -rf /usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcampax.github.com
