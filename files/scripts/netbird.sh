461ebe3b46381c9f230b35dd9b01ea7b79ae9914
dadaxer <dadaxer@mailbox.org>
2025-07-26 11:55:40 +0200

    Update netbird.sh

--- files/scripts/netbird.sh
+++ files/scripts/netbird.sh
@@ -7,16 +7,3 @@ set -oue pipefail
 
 # Install netbird vpn using the official installation script
 curl -fsSL https://pkgs.netbird.io/install.sh | sh
-
-# tee /etc/yum.repos.d/netbird.repo <<EOF
-# [netbird]
-# name=netbird
-# baseurl=https://pkgs.netbird.io/yum/
-# enabled=1
-# gpgcheck=0
-# gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
-# repo_gpgcheck=1
-# EOF
-
-# dnf config-manager addrepo -y --overwrite --from-repofile=/etc/yum.repos.d/netbird.repo
-# dnf install netbird -y
