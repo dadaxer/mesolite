61f7dc35ab1b1e6eb293eb82d5037bb033a4172e
dadaxer <dadaxer@mailbox.org>
2025-07-26 11:53:21 +0200

    **Subject:** Install Netbird VPN in build scripts
    **Body:**
    
    - Remove unnecessary echo statements and YUM repo configuration - Add Netbird installation using
    official script

--- files/scripts/netbird.sh
+++ files/scripts/netbird.sh
@@ -5,19 +5,18 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Your code goes here.
-echo 'This is an example shell script'
-echo 'Scripts here will run during build if specified in recipe.yml'
+# Install netbird vpn using the official installation script
+curl -fsSL https://pkgs.netbird.io/install.sh | sh
 
-tee /etc/yum.repos.d/netbird.repo <<EOF
-[netbird]
-name=netbird
-baseurl=https://pkgs.netbird.io/yum/
-enabled=1
-gpgcheck=0
-gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
-repo_gpgcheck=1
-EOF
+# tee /etc/yum.repos.d/netbird.repo <<EOF
+# [netbird]
+# name=netbird
+# baseurl=https://pkgs.netbird.io/yum/
+# enabled=1
+# gpgcheck=0
+# gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
+# repo_gpgcheck=1
+# EOF
 
-dnf config-manager addrepo -y --overwrite --from-repofile=/etc/yum.repos.d/netbird.repo
-dnf install netbird -y
+# dnf config-manager addrepo -y --overwrite --from-repofile=/etc/yum.repos.d/netbird.repo
+# dnf install netbird -y
