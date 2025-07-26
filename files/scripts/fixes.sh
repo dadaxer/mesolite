53e3b4bac1c1e335226925622b160e4ccc27ee73
dadaxer <dadaxer@mailbox.org>
2025-07-26 11:40:37 +0200

    Update fixes.sh

--- files/scripts/fixes.sh
+++ files/scripts/fixes.sh
@@ -3,4 +3,7 @@
 set -oue pipefail
 
 # gnome-extensions module can't remove "Window List", so manual deletion it is.
-rm -rf /usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcampax.github.com
+if [ -d "/usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcampax.github.com" ]; then
+  echo "Removing 'Window List' extension..."
+  rm -rf /usr/share/gnome-shell/extensions/window-list@gnome-shell-extensions.gcampax.github.com || { echo "Failed to remove 'Window List' extension"; exit 1; }
+fi
