3acb38cf3f76179a353244e573b2a5bfc089d951
dadaxer <dadaxer@mailbox.org>
2025-08-05 08:53:52 +0000

    Enable Flatpak First Setup Service
    Add new systemd service and preset for automatically setting up Flatpak
    applications on first boot.

---
+++ files/system/usr/share/ublue-os/flatpak-first-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+flatpak --user remote-add --if-not-exists flathub --title "Flathub (user)" https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak --user install -y com.github.tchx84.Flatseal \
+                        com.mattjakeman.ExtensionManager \
+                        io.missioncenter.MissionCenter \
+                        io.github.flattool.Warehouse \
+                        org.mozilla.firefox \
+                        org.mozilla.Thunderbird \
+                        page.tesk.Refine
