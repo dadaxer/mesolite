f9664cbddff732ab6e629f9f6170270d3377586b
dadaxer <dadaxer@mailbox.org>
2025-08-05 21:15:52 +0000

    Update build configuration and remove unused files
    - Updated `.github/workflows/build.yml` to ensure consistent naming and
    structure - Renamed `recipes/mesolite-stable.yml` to
    `recipes/recipe.yml` - Removed unused files
    `files/system/etc/dconf/db/distro.d/01-bluefin-folders`,
    `files/system/etc/dconf/db/distro.d/02-bluefin-keybindings`, and
    `files/system/etc/dconf/db/distro.d/05-bluefin-searchlight-extension` -
    Updated `recipes/modules/packages-dnf.yml` to comment out unused
    packages

--- files/scripts/bling.sh
+++ files/scripts/bling.sh
@@ -12,16 +12,16 @@ rm /usr/share/ublue-os/bluefin-logos/symbols/karl
 rm /usr/share/ublue-os/bluefin-logos/sixels/karl
 echo 'Karl removed'
 
-echo 'Removing backgrounds...'
-rm -rf /usr/share/backgrounds/bluefin
-rm -rf /usr/share/gnome-background-properties/*bluefin.xml
-rm -rf /usr/share/gnome-background-properties/xe_space_needle.xml
-rm -rf /usr/share/gnome-background-properties/chicken.xml
-rm -rf /usr/share/gnome-background-properties/xe_clouds.xml
-rm -rf /usr/share/gnome-background-properties/xe_foothills.xml
-rm -rf /usr/share/gnome-background-properties/xe_space_needle.xml
-rm -rf /usr/share/gnome-background-properties/xe_sunset.xml
-echo 'Backgrounds removed'
+# echo 'Removing backgrounds...'
+# rm -rf /usr/share/backgrounds/bluefin
+# rm -rf /usr/share/gnome-background-properties/*bluefin.xml
+# rm -rf /usr/share/gnome-background-properties/xe_space_needle.xml
+# rm -rf /usr/share/gnome-background-properties/chicken.xml
+# rm -rf /usr/share/gnome-background-properties/xe_clouds.xml
+# rm -rf /usr/share/gnome-background-properties/xe_foothills.xml
+# rm -rf /usr/share/gnome-background-properties/xe_space_needle.xml
+# rm -rf /usr/share/gnome-background-properties/xe_sunset.xml
+# echo 'Backgrounds removed'
 
 # echo 'Removing vscode files...'
 # rm -rf /etc/skel/.config/Code
