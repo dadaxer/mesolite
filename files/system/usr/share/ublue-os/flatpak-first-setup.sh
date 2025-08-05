818b610eb4fb0dc02f2eef4a5b2133aec8747fe7
dadaxer <dadaxer@mailbox.org>
2025-08-05 11:00:44 +0000

    Update `flatpak-first-setup.sh` with additional applications: - com.github.rafostar.Clapper - com.heroicgameslauncher.hgl - com.valvesoftware.Steam - io.github.Faugus.faugus-launcher - org.gnome.World.PikaBackup - org.gnome.Papers - org.gnome.clocks - sh.loft.devpod - com.ranfdev.DistroShelf - com.discordapp.Discord
    Reorder and simplify the application list.

--- files/system/usr/share/ublue-os/flatpak-first-setup.sh
+++ files/system/usr/share/ublue-os/flatpak-first-setup.sh
@@ -5,9 +5,24 @@ set -oue pipefail
 flatpak --user remote-add --if-not-exists flathub --title "Flathub (user)" https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak --user install -y com.github.tchx84.Flatseal \
+                        com.github.rafostar.Clapper \
+                        com.heroicgameslauncher.hgl \
                         com.mattjakeman.ExtensionManager \
-                        io.missioncenter.MissionCenter \
+                        com.valvesoftware.Steam \
+                        io.github.Faugus.faugus-launcher \
                         io.github.flattool.Warehouse \
+                        io.missioncenter.MissionCenter \
+                        io.podman_desktop.PodmanDesktop \
+                        org.gnome.World.PikaBackup \
+                        org.gnome.Papers \
+                        org.gnome.clocks \
                         org.mozilla.firefox \
                         org.mozilla.Thunderbird \
-                        page.tesk.Refine
+                        page.tesk.Refine \
+                        sh.loft.devpod \
+                        com.ranfdev.DistroShelf \
+                        com.discordapp.Discord \
+                        com.github.tchx84.Flatseal \
+                        org.gnome.baobab \
+                        org.gnome.gThumb \
+                        org.freedesktop.Platform.VulkanLayer.MangoHud
