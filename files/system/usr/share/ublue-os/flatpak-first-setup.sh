792d6f287bb1ebf1bbdf79861530b56d0a3cbe52
dadaxer <dadaxer@mailbox.org>
2025-08-05 09:42:13 +0000

    Add flatpak first setup service and configuration
    Introduce a new systemd service for handling Flatpak first-time setup,
    along with a tmpfiles.d entry to ensure the script has the correct
    permissions. The script is now executed using `sh` for better
    compatibility and robustness.
    
    The commit includes: - A new systemd service file in
    `/usr/lib/systemd/user/flatpak-first-setup.service` - A new tmpfiles.d
    configuration file in `/usr/lib/tmpfiles.d/flatpak-first-setup.conf` -
    An updated script with error handling and execution wrapper

--- files/system/usr/share/ublue-os/flatpak-first-setup.sh
+++ files/system/usr/share/ublue-os/flatpak-first-setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+set -oue pipefail
+
 flatpak --user remote-add --if-not-exists flathub --title "Flathub (user)" https://dl.flathub.org/repo/flathub.flatpakrepo
 
 flatpak --user install -y com.github.tchx84.Flatseal \
