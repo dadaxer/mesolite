a9401440e60c725dcaf83a52dc1c577809baec1f
dadaxer <dadaxer@mailbox.org>
2025-08-06 06:35:21 +0000

    Remove unused scripts and background removal from bling.sh Add VictorMono and Mononoki fonts to the recipe

--- files/scripts/bling.sh
+++ files/scripts/bling.sh
@@ -11,20 +11,3 @@ rm /usr/share/ublue-os/bluefin-logos/karl.png
 rm /usr/share/ublue-os/bluefin-logos/symbols/karl
 rm /usr/share/ublue-os/bluefin-logos/sixels/karl
 echo 'Karl removed'
-
-# echo 'Removing backgrounds...'
-# rm -rf /usr/share/backgrounds/bluefin
-# rm -rf /usr/share/gnome-background-properties/*bluefin.xml
-# rm -rf /usr/share/gnome-background-properties/xe_space_needle.xml
-# rm -rf /usr/share/gnome-background-properties/chicken.xml
-# rm -rf /usr/share/gnome-background-properties/xe_clouds.xml
-# rm -rf /usr/share/gnome-background-properties/xe_foothills.xml
-# rm -rf /usr/share/gnome-background-properties/xe_space_needle.xml
-# rm -rf /usr/share/gnome-background-properties/xe_sunset.xml
-# echo 'Backgrounds removed'
-
-# echo 'Removing vscode files...'
-# rm -rf /etc/skel/.config/Code
-# rm /etc/yum.repos.d/vscode.repo
-# rm /usr/share/ublue-os/user-setup.hooks.d/10-vscode.sh
-# echo 'files removed'
